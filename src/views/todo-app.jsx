956923689803a26f56c99d42561ba1e3f82c2934
Amit <amitrozen2015@gmail.com>
2022-09-20 16:30:13 +0300

    starter

---
+++ src/views/todo-app.jsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+
+
+const TodoApp = () => {
+
+    useEffect(() => {
+
+    }, [])
+
+
+
+
+    return (
+        <main className="main-container">
+            <div className="main-left">left</div>
+            <div className="main-main">Main app</div>
+            <div className="main-right">right</div>
+        </main>
+    )
+}
+
+export default TodoApp;
